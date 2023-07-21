b87abb28ad00728b42c47dbd56a4eb00936eaa67
ali <ali.haydaroglu@mail.utoronto.ca>
2023-07-21 19:09:37 +0100

    better tif plotting function

--- utils/tiff_utils.py
+++ utils/tiff_utils.py
@@ -14,41 +14,76 @@ from suite2p.io import lbm as lbmio
 import json
 from matplotlib import pyplot as plt
 
-def show_tif(im, flip=1, cmap='Greys_r', colorbar=False, other_args = {},figsize=(8,6), dpi=150, alpha=None,
-             ticks=False, ax = None, px_py=None, exact_pixels=False, vminmax_percentile = (0.5,99.5), vminmax = None):
-    f = None
+def show_tif(im, flip=1, cmap='Greys_r', colorbar=False, other_args = {},figsize=(8,6), dpi=150, alpha=None, return_fig=True,
+             ticks=False, ax = None, px_py=None, exact_pixels=False, vminmax_percentile = (0.5,99.5), vminmax = None, facecolor='white', xticks=None, yticks = None,
+             norm=None, cbar=False, cbar_loc='left', cbar_ori='vertical', cbar_title='', interpolation='nearest', ax_off=False, cax_kwargs={'frameon':False}):
 
+    f = None
+    im = im.copy()
     if exact_pixels:
         ny, nx = im.shape
         figsize = (nx / dpi, ny / dpi)
         px_py = None
-
+    
+    new_args = {}
+    new_args.update(other_args)
     if ax is None: f,ax = plt.subplots(figsize=figsize, dpi=dpi)
-    ax.grid(False)
-    if 'interpolation' not in other_args.keys():
-        other_args['interpolation'] = 'nearest'
-    if vminmax_percentile is not None and vminmax is None:
-        other_args['vmin'] = n.percentile(im, vminmax_percentile[0])
-        other_args['vmax'] = n.percentile(im, vminmax_percentile[1])
-    if vminmax is not None:
-        other_args['vmin'] = vminmax[0]
-        other_args['vmax'] = vminmax[1]
 
+    if facecolor is not None:
+        ax.set_facecolor(facecolor)
+    ax.grid(False)
+    new_args['interpolation'] = interpolation
+    if vminmax_percentile is not None and vminmax is None:
+        non_nan = ~n.isnan(im)
+        new_args['vmin'] = n.percentile(im[non_nan], vminmax_percentile[0])
+        new_args['vmax'] = n.percentile(im[non_nan], vminmax_percentile[1])
+    if vminmax is not None:
+        new_args['vmin'] = vminmax[0]
+        new_args['vmax'] = vminmax[1]
     if px_py is not None:
-        other_args['aspect'] = px_py[1]/px_py[0]
+        new_args['aspect'] = px_py[1]/px_py[0]
     if alpha is not None:
-        other_args['alpha'] = alpha
-    im = ax.imshow(flip*im,cmap=cmap, **other_args)
+        new_args['alpha'] = alpha.copy()
+    if norm is not None:
+        new_args['norm'] = norm
+        new_args['vmin'] = None; new_args['vmax'] = None
+    # print(new_args)
+    axim = ax.imshow(flip*im,cmap=cmap, **new_args)
     if colorbar: plt.colorbar()
     if not ticks:
         ax.set_xticks([]); ax.set_yticks([]);
     if exact_pixels:
         plt.subplots_adjust(left=0,right=1,bottom=0,top=1)
     # plt.tight_layout()
-    return f, ax, im
+    if norm: 
+        new_args['vmin'] = norm.vmin; new_args['vmax'] = norm.vmax
+    if cbar:
+        if cbar_loc == 'left':
+            cbar_loc = [0.025, 0.4, 0.02, 0.2]; cbar_ori='vertical'
+        elif cbar_loc == 'right':
+            cbar_loc = [0.88, 0.4, 0.02, 0.2]; cbar_ori='vertical'
+        elif cbar_loc == 'top':
+            cbar_loc = [0.4, 0.95, 0.2, 0.02]; cbar_ori='horizontal'
+        elif cbar_loc == 'bottom':
+            cbar_loc = [0.4, 0.05, 0.2, 0.02]; cbar_ori='horizontal'
+        cax = ax.inset_axes(cbar_loc, **cax_kwargs)
+        plt.colorbar(axim, cax=cax, orientation=cbar_ori)
+        if cbar_ori == 'vertical':
+            cax.set_yticks([new_args['vmin'], new_args['vmax']],['%.2f'% new_args['vmin'], '%.2f' % new_args['vmax']], color='k', fontsize=9)
+            cax.set_ylabel(cbar_title, color='k', fontsize=9,labelpad=-13)
+        if cbar_ori == 'horizontal':
+            cax.set_xticks([new_args['vmin'], new_args['vmax']],['%.2f' % new_args['vmin'], '%.2f' % new_args['vmax']], color='k', fontsize=9)
+            cax.set_xlabel(cbar_title, color='k', fontsize=9,labelpad=-13)
+    if xticks is not None:
+        ax.set_xticks(range(len(xticks)), xticks)
+    if yticks is not None:
+        ax.set_yticks(range(len(yticks)), yticks)
+    if ax_off:
+        ax.axis('off')
 
+    if return_fig: return f, ax, axim
 
-def get_tif_paths(dir_path, regex_filter=None):
+def get_tif_paths(dir_path, regex_filter=None, sort=True):
     '''
     Get a list of absolute paths for all tif files in this directory
 
@@ -70,6 +105,7 @@ def get_tif_paths(dir_path, regex_filter=None):
         tif_paths = tif_paths_filtered
 
     # print(tif_paths)
+    if sort: tif_paths = list(n.sort(tif_paths))
     return (tif_paths)
 
 def get_meso_rois(tif_path, max_roi_width_pix=145):
